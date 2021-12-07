8dd6ea6a68750e2245697e3637a49aff0b78f80c
gori2607 <zm19539@josai.ac.jp>
2021-12-07 02:01:32 +0000

    1207

---
+++ ex06-10.js
@@ -0,0 +1,8 @@
+function imgchange1() {
+document.getElementById('logo').src= 
+'./image/josailogo.jpg';
+}
+function imgchange2() {
+    document.getElementById('logo').src= 
+    './image/keiei_small.jpg';
+    }
